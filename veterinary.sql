3360a467d11d407f6ba2a1943da76614b179de75
Avryxxxx <nesnesjalandoni@gmail.com>
2023-11-10 12:51:41 +0800

    conflict

--- veterinary.sql
+++ veterinary.sql
@@ -135,8 +135,12 @@ VALUES
 ALTER TABLE owners
 ADD COLUMN registereddate DATE;
 
-ALTER TABLE invoices
-RENAME COLUMN paymentdate TO paymenttime;
 
-
+DELETE FROM invoices
+WHERE appointid = (
+	SELECT appointid 
+	FROM appointments 
+	WHERE animalid = (
+		SELECT animalid 
+		FROM animals WHERE name = 'Simba'));
 
