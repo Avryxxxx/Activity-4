f4a1cdf2455087ea9588b9eed7de931ae7e22cdf
JANNA_KRISTINE BAYANIN <jannakristinebayanin@gmail.com>
2023-11-10 12:51:55 +0700

    conflict

--- veterinary.sql
+++ veterinary.sql
@@ -153,5 +153,5 @@ WHERE animalid = (
 SET dlastname = 'Reyes-Gonzales'
 WHERE dfirstname = 'Dr. Sofia';
 
-SELECT DISTINCT species
-FROM animals;
+SELECT SUM(totalamount) AS total_sales
+FROM invoices;
